24024bd4c8bcdc2461d6fab7e851aa528426e57b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-26 20:09:14 +0700

    preventing duplicate user

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -3,11 +3,17 @@ const User = require('../models/user');
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
 
+  const oldUser = await User.findOne({ email });
+
+  if (oldUser) {
+    return res.status(401).json({ error: 'This email is already in use' });
+  }
+
   const newUser = new User({ name, email, password });
 
   await newUser.save();
 
-  res.json({ user: newUser });
+  res.status(201).json({ user: newUser });
 };
 
 module.exports = {
--- backend/models/user.js
+++ backend/models/user.js
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema({
     type: String,
     trim: true,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
