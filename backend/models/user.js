b5632499b838b4a904556535f2c5394f35d651c6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-26 17:45:26 +0700

    mongoose model

---
+++ backend/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
