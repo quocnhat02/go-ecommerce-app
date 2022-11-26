015a6cfef86bcbb68db5be8412dcdbe44228a865
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-26 20:17:54 +0700

    middleware inside express

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,22 @@ app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
 
+app.post(
+  '/api/v1/sign-in',
+  (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.json({ error: 'email/password missing!' });
+    }
+
+    next();
+  },
+  (req, res) => {
+    return res.json({ user: req.body });
+  }
+);
+
 app.listen(8000, () => {
   console.log('The port is listening on 8000');
 });
