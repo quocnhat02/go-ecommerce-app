9411d71870429bc3c7207b005439a508c4d89392
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-12 16:39:11 +0700

    gin router 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/quocnhat02/go-ecommerce-app/internal/routers"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	r := routers.NewRouter()
+	r.Run(":8002")
 }
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,1 +1,18 @@
 package routers
+
+import "github.com/gin-gonic/gin"
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping/:name", Pong)
+	return r
+}
+
+func Pong(c *gin.Context) {
+	uid := c.DefaultQuery("uid", "123")
+	name := c.Param("name")
+	c.JSON(200, gin.H{
+		"message": "pong" + name,
+		"uid":     uid,
+	})
+}
